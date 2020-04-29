f2939e5eeedd44ee34d0645085be036ce37e5d8e
suzukalight <mail@mkubara.com>
2020-04-29 23:42:19 +0900

    Refactor modules

--- main.tf
+++ main.tf
@@ -6,55 +6,12 @@ variable "example_instance_type" {
   default = "t3.micro"
 }
 
-data "aws_ami" "recent_amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
+module "web_server" {
+  source        = "./http_server"
+  instance_type = var.example_instance_type
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
 }
 
-resource "aws_security_group" "example_ec2" {
-  name = "example_ec2"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "example" {
-  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type          = var.example_instance_type
-  vpc_security_group_ids = [aws_security_group.example_ec2.id]
-
-  user_data = <<EOF
-  #!/bin/bash
-  yum install -y httpd
-  systemctl start httpd.service
-EOF
-}
-
-output "ami_image_id" {
-  value = data.aws_ami.recent_amazon_linux_2.image_id
-}
-
-output "example_public_dns" {
-  value = aws_instance.example.public_dns
+output "public_dns" {
+  value = module.web_server.public_dns
 }
