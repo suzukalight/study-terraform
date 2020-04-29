334f184c4bfa3e5a71e57af2a821bfd0ecf44568
suzukalight <mail@mkubara.com>
2020-04-29 23:07:53 +0900

    Add variables, data and output

--- main.tf
+++ main.tf
@@ -1,13 +1,37 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  ami           = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type = var.example_instance_type
 
   user_data = <<EOF
   #!/bin/bash
   yum install -y httpd
   systemctl start httpd.service
 EOF
+}
+
+output "ami_image_id" {
+  value = data.aws_ami.recent_amazon_linux_2.image_id
 }
