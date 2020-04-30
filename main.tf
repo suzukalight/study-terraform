51a82f3377568087900a15d4a2ddac44881aa7cd
suzukalight <mail@mkubara.com>
2020-04-30 10:03:51 +0900

    Use iam_role module

--- main.tf
+++ main.tf
@@ -12,6 +12,21 @@ module "web_server" {
 
 }
 
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
 output "public_dns" {
   value = module.web_server.public_dns
 }
